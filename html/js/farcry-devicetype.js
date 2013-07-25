0305efcce8b0b3f0920d42224608fb786b50bec8
Geoff Bowers <modius@daemon.com.au>
2013-07-25 21:41:32 +1000

    Initial commit.

---
+++ html/js/farcry-devicetype.js
@@ -0,0 +1,3 @@
+$j(function(){function switchDeviceType(deviceType){setDeviceTypeCookie(deviceType);window.location=window.location;}
+function setDeviceTypeCookie(deviceType){var date=new Date();date.setTime(date.getTime()+(30*24*60*60*1000));document.cookie="FARCRYDEVICETYPE="+deviceType+"; expires="+date.toGMTString()+"; path=/;";}
+$j(".fc-switch-device-desktop").live("click",function(){switchDeviceType("desktop");});$j(".fc-switch-device-mobile").live("click",function(){switchDeviceType("mobile");});$j(".fc-switch-device-tablet").live("click",function(){switchDeviceType("tablet");});});
